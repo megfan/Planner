6d4be7d149fc4c496b484d51a58e59d36400e2da
megfan <megfa@yahoo.com>
2019-01-19 14:53:48 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
 // import './App.css';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 
+import Navbar from './components/layout/Navbar';
+import Dashboard from "./components/dashnoard/Dashboard";
+import ProjectDetails from "./components/projects/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SighUp";
+import CreateProject from "./components/projects/CreateProject";
 
 class App extends Component {
   render() {
@@ -10,6 +15,13 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
+          <Switch>
+            <Route path="/" component={Dashboard} exact />
+            <Route path="/project/:id" component={ProjectDetails} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/create" component={CreateProject} />
+          </Switch>
         </div>
       </BrowserRouter>    
     );
