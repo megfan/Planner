7b38f2710be3ab3e05db207b18bb1fcba3acc498
megfan <megfa@yahoo.com>
2019-02-02 10:42:53 +0100

    Updates

---
+++ precache-manifest.7aed3d69c2ca9387010d258c0f5738ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "722dca14daf2d0774c02",
+    "url": "/Planner/static/js/runtime~main.722dca14.js"
+  },
+  {
+    "revision": "10c91af4ddbab64e6c66",
+    "url": "/Planner/static/js/main.10c91af4.chunk.js"
+  },
+  {
+    "revision": "ff023a5af87f95c05fff",
+    "url": "/Planner/static/js/1.ff023a5a.chunk.js"
+  },
+  {
+    "revision": "10c91af4ddbab64e6c66",
+    "url": "/Planner/static/css/main.749ad260.chunk.css"
+  },
+  {
+    "revision": "13fe2b4ec0eaa571cf7d9394cfbb9ed6",
+    "url": "/Planner/index.html"
+  }
+];
